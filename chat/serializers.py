945ed13c0d00d91b10aebe06a58e3265ab374a71
manveer11011 <manveer11011@outlook.com>
2025-07-29 01:16:51 +0530

    everything is well

--- chat/serializers.py
+++ chat/serializers.py
@@ -3,7 +3,7 @@ from .models import Message
 from django.contrib.auth.models import User
 
 class MessageSerializer(serializers.ModelSerializer):
-    sender = serializers.StringRelatedField(read_only=True)  # Show username instead of ID
+    sender = serializers.StringRelatedField(read_only=True)  # Show username id di tha
     receiver = serializers.SlugRelatedField(queryset=User.objects.all(), slug_field='username')
 
     class Meta:
