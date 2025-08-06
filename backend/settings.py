c6f806af26cf12630a4d88665cecce1f148467ee
manveer11011 <manveer11011@outlook.com>
2025-08-07 00:39:12 +0530

    tezst

--- backend/settings.py
+++ backend/settings.py
@@ -169,7 +169,7 @@ SPECTACULAR_SETTINGS = {
 }
 
 CORS_ALLOWED_ORIGINS = [
-    "https://chat-app-frontend-git-main-ravis-projects-dd0ae145.vercel.app",                  # Vite dev
+    "https://chat-app-frontend-coral-zeta.vercel.app/login/login",                  # Vite dev
     # "https://<your-frontend-domain>",         # Vercel/Netlify if deployed
 ]
 import os
